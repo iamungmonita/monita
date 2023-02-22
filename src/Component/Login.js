0972904a8b487a43663faa7da4d24c01cf4c6055
Monita Ung <iamungmonita@gmail.com>
2023-02-22 17:58:34 +0900

    login

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import style from '/Users/ungmonita/my-website/src/Style/Login.module.scss'
-import Register from './Register'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react'
 const Login = () => {
+const inputRef = useRef()
+  const submit = (e) => {
+    e.preventDefault()
+    console.log(inputRef.current.value);
+    inputRef.current.value = ''
+  }
   return (
-    <div className={ `bg  ${style.green}` }>
-      <h2>Login</h2>
-      <Link to='/Register'>Register</Link>   
+   <div className="login">
+      <div className={ `${style.login} login` }>
+      <h2>Sign in to deft</h2>
+
+      <form action="" onSubmit={submit}>
+        <div className={`${style.url}`}>
+        <label htmlFor="">Your deft URL</label>
+        <input type="email" placeholder="your email" ref={inputRef}/>
+        </div>
+        <div className={`${style.right}`}>
+          <a href="#">Forget your name? </a>
+        </div>
+        <div>
+          <input className='btn-primary' type="submit" value='Sign In' />
+        </div>
+        <div>
+        <Link to='/Register'> <input className={`btn-secondary ${style.border}`} type="submit" value='New here? Start free trial?' /></Link>   
+        </div>
+      </form>
+  
     </div>
+   </div>
   )
 }
 
--- src/Component/Register.js
+++ src/Component/Register.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className={`bg`}>
+    <div>
       <h2>Register</h2>
       <form action="">
         <input type="text" placeholder='name' />
